52a67b65aebd67e0d63b246b3c8ac71782beffce
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2022-11-11 02:01:22 -0500

    Fixed Things

--- squirrel/buildDebug.bat
+++ squirrel/buildDebug.bat
@@ -2,8 +2,7 @@
 cls
 
 set releasePath=%cd%\..\Release
-set buildPath=%cd%\..\src\Move Generator\bin\Debug\net6.0-windows
 
-Squirrel pack --packId "Move_Generator" --packTitle "Move Generator" --packAuthors "TheCrafters001" --packVersion "0.0.1" --framework net6.0 --packDirectory "%buildPath%" --releaseDir %releasePath%
+Squirrel pack --packId "Move-Generator" --packTitle "Move Generator" --packAuthors "TheCrafters001" --packVersion "1.0.0" --framework net6.0 --packDirectory "..\src\Move Generator\bin\Release\net6.0-windows" --releaseDir %releasePath%
 
 pause
