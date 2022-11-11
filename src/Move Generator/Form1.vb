ebc071665f1df349abce9097bc3347104b01d904
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2022-11-11 01:53:59 -0500

    Update to add Squirrel Code

--- src/Move Generator/Form1.vb
+++ src/Move Generator/Form1.vb
@@ -1,5 +1,11 @@
-﻿Public Class Form1
+﻿Imports Squirrel
+
+Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        SquirrelAwareApp.HandleEvents(onInitialInstall:=AddressOf OnAppInstall, onAppUninstall:=AddressOf OnAppUninstall, onEveryRun:=AddressOf OnAppRun)
+        UpdateMyApp()
+
         category_cmbBox.SelectedIndex = 0
         targets_cmbBox.SelectedIndex = 0
     End Sub
@@ -7,4 +13,27 @@
     Private Sub generate_btn_Click(sender As Object, e As EventArgs) Handles generate_btn.Click
         Generator.CreateMove()
     End Sub
+
+
+    Private Shared Sub OnAppInstall(ByVal version As SemanticVersion, ByVal tools As IAppTools)
+        tools.CreateShortcutForThisExe(ShortcutLocation.StartMenu Or ShortcutLocation.Desktop)
+    End Sub
+
+    Private Shared Sub OnAppUninstall(ByVal version As SemanticVersion, ByVal tools As IAppTools)
+        tools.RemoveShortcutForThisExe(ShortcutLocation.StartMenu Or ShortcutLocation.Desktop)
+    End Sub
+
+    Private Shared Sub OnAppRun(ByVal version As SemanticVersion, ByVal tools As IAppTools, ByVal firstRun As Boolean)
+        tools.SetProcessAppUserModelId()
+    End Sub
+
+    Private Shared Async Function UpdateMyApp() As Task
+        Dim mgr = New GithubUpdateManager("https://github.com/TheCrafters001/Move-Generator/releases/latest")
+
+        Dim newVersion = Await mgr.UpdateApp()
+
+        If newVersion IsNot Nothing Then
+            UpdateManager.RestartApp()
+        End If
+    End Function
 End Class
